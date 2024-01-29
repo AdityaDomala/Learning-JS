77e5c147921c20165591c00136530c498efee3d7
AdityaDomala <56511886+AdityaDomala@users.noreply.github.com>
2024-01-29 05:51:51 +0000

    Conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+//console.log(typeof (score));
+//console.log(typeof (score));
+
+let valueInNumberBe = Number(score)
+//console.log(typeof valueInNumberBe);
+//console.log(valueInNumberBe);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let ThePersonIsLoggedIn = "true"
+let TheBooleanIsLoggedIn = Boolean(ThePersonIsLoggedIn) 
+//console.log(TheBooleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
