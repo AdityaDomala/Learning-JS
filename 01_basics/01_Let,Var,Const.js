f7c177d16dd9dcff6f4c367ea89a867cbbd657a7
AdityaDomala <56511886+AdityaDomala@users.noreply.github.com>
2024-01-29 03:49:52 +0000

    datatyes and ECMA

---
+++ 01_basics/01_Let,Var,Const.js
@@ -0,0 +1,17 @@
+const studentId = "02096198"
+let studentCourse = "ADM" //type 1 recommended
+var studentFees =  "3650" //tye 2 not recommended
+studentPresent = "No"
+
+studentFees = "4000" // type c depends on coder 
+
+let studentCity;
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(studentFees);
+console.table([studentId,studentCourse,studentFees,studentCity]);
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Learning-JS
 Learning JS 
+
+Here we have used codespaces for coding and learning JS, We have also installed/established Nodejs + JS environment, by going through command palette and choosing container and then adding node js
