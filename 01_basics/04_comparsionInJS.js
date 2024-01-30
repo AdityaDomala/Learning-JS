ed83206e9f53c7dc6cb3d1e1e34c33c747eba87e
AdityaDomala <56511886+AdityaDomala@users.noreply.github.com>
2024-01-30 20:56:17 +0000

    comparsionInJs

---
+++ 01_basics/04_comparsionInJS.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);   // No problem in these type of comparsions
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);   // Problem in these type of comparsions is using string 
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === 
+
+// console.log("2" === 2);
